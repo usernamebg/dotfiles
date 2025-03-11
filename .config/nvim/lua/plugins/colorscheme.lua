f72f378bfca26c54edb31f329b278b9871d7dc37
bgalchev <galchevbobi@gmail.com>
2025-03-11 22:17:12 +0200

    Testing New Colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,18 @@
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
 		config = function()
-			require("rose-pine").setup()
+			require("tokyonight").setup({
+				style = "night", -- Options: storm, night, moon, day
+				transparent = true, -- This prevents the theme from setting a background color
+			})
 
-			vim.cmd("colorscheme rose-pine")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+			vim.cmd("colorscheme tokyonight")
+
+			-- Force black background
+			vim.api.nvim_set_hl(0, "Normal", { bg = "#000000", ctermbg = 0 })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000", ctermbg = 0 })
 		end,
 	},
 }
